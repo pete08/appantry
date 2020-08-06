68f4ba8b8daeb907bab8e5b7262ba018ad0e12fe
PeterH <pete08@gmail.com>
2020-08-06 09:19:41 -0500

    Add user_item API CREATE action, show.json.jb file

--- app/controllers/api/user_items_controller.rb
+++ app/controllers/api/user_items_controller.rb
@@ -5,4 +5,16 @@ class Api::UserItemsController < ApplicationController
     render "index.json.jb"
   end
 
+  def create
+    UserItem.create(
+      item_id: params[:item_id],
+      user_id: params[:user_id],
+      buy_date: params[:buy_date],
+      used: false,
+      future_interest: true
+    )
+    @user_item = UserItem.last
+    render "show.json.jb"
+  end
+
 end

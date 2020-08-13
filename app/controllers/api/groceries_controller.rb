5a758d2b3501c0eadb2694326524a4642077d26a
PeterH <pete08@gmail.com>
2020-08-12 21:09:58 -0500

    Add groceries index route

--- app/controllers/api/groceries_controller.rb
+++ app/controllers/api/groceries_controller.rb
@@ -1,2 +1,11 @@
 class Api::GroceriesController < ApplicationController
+  before_action :authenticate_user
+
+  def index
+    @user_items = UserItem.where(user_id: current_user.id, used: "true", future_interest: "true")
+
+    render "index.json.jb"
+  end
+  
+
 end

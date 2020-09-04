c16be9b90fe7f1e0d3957753f053573c49f0c48e
PeterH <pete08@gmail.com>
2020-09-03 21:43:20 -0500

    Add spoonacular api to get recipes , steps, including the haves and need ingreds based on hard coded ingredrients

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,30 +1,91 @@
 class Api::RecipesController < ApplicationController
 
   def index
-    @user_items = UserItem.where(user_id: current_user.id) 
+    # @user_items = UserItem.where(user_id: current_user.id) 
     p "*" * 45
-    @user_items = @user_items.where(used: "false", future_interest: "true")
+    # @user_items = @user_items.where(used: "false", future_interest: "true")
     api_key = Rails.application.credentials.spoon_api[:api_key]
-
-    @recipes = HTTP
-      .get("https://api.spoonacular.com/recipes/findByIngredients?apiKey=#{api_key}&ingredients=apples,flour,sugar&number=3")
+    count = 2
+    @data = HTTP
+      .get("https://api.spoonacular.com/recipes/findByIngredients?apiKey=#{api_key}&ingredients=eggs,flour,milk,chocolate&number=#{count}")
       .parse
     
+    p "*" * 45
+    p "*" * 45
+    p @data.class
+    p "*" * 45
+    p "*" * 45
+    p "@data[0][\"id\"]"
+    p @data[0]["id"]
+    p "@data[0][\"title\"]"
+    p @data[0]["title"]
+  
+    @recipes_list = []
+    @data.each do |recipe|
+      recipe_temp = {}
+      id = recipe["id"]
+      recipe_temp[:id] = recipe["id"]
+      recipe_temp[:image] = recipe["image"]
+      recipe_temp[:title] = recipe["title"]
 
-    @recipes.each do |recipe|
-      {
-      title: recipe["title"],
-      image: recipe["image"],
-      needIng: recipe["missedIngredients"],
-      haveIng: recipe["usedIngredients"],
-    }
+      need = recipe["missedIngredients"]
+      need_temp = []
+      
+      if need
+        need.each do |ingred|
+          if ingred["name"] 
+            need_temp << ingred["name"]
+          end
+        end
+      end
+      recipe_temp[:need] = need_temp
+
+
+      have = recipe["usedIngredients"],
+      have_temp = []
+
+      if recipe["usedIngredients"]
+        recipe["usedIngredients"].each do |ingred|
+          if ingred["name"] 
+            have_temp << ingred["name"]
+          end
+        end
+      end
+      recipe_temp[:have] = have_temp
+
+
+      @recipe_directions = HTTP
+        .get("https://api.spoonacular.com/recipes/#{id}/analyzedInstructions?apiKey=#{api_key}")
+        .parse
+
+      recipe_steps = @recipe_directions[0]["steps"]
+      steps_temp = []
+      recipe_steps.each do |step|
+        steps_temp << step["step"]
+      end
+      
+      recipe_temp[:steps]  = steps_temp
+
+      
+      
+      @recipes_list.append(recipe_temp)
     end
+    
 
 
+    ######################Get REcipe Instructions Per Recipe ID########USE Count to track which recipe goes with recipe instructions##############
+    # @recipes_ids = []                               # List of recipe Ids
+    # @recipes.each do |recipe|
+    #   recipes_ids["id"] = recipe[:id]
+    # @recipes_instructions = HTTP                  # Get API Call for recipe per recipe Id from @recipes_ids
+    #   .get("https://api.spoonacular.com/recipes/611026/analyzedInstructions?apiKey=#{api_key}")
+    #   .parse
+    #################################################################################
 
     
     render "index.json.jb"
     
   end
+  
   
 end
